e0751f184324011230d0fbda1e89d7f62fc067e3
Darren Britton <darrenbritton@hotmail.com>
2016-03-14 16:24:23 +0000

    refactored preprocessing

--- C12731135+C12425568.py
+++ C12731135+C12425568.py
@@ -27,12 +27,10 @@ def get_data(file):
     cat_data = data.drop(cont_features + ["y"], axis=1)
     start = True
     for cat in cat_features:
-        cat_data_temp = cat_data[cat]
-        cat_data_temp_series = cat_data_temp.to_frame(cat)
-        # cat_data_temp
-        cat_data_temp_series = cat_data_temp_series.T.to_dict().values()
+        cat_data_temp = cat_data[cat].to_frame(cat)
+        cat_data_temp = cat_data_temp.T.to_dict().values()
         vectorizer = DictVectorizer(sparse=False)
-        vec_cat_data = vectorizer.fit_transform(cat_data_temp_series)
+        vec_cat_data = vectorizer.fit_transform(cat_data_temp)
 
         if start:
             cat_data_trans = vec_cat_data
